5d73aa3cd3bf86d6551bfbd5627a81f38d8db8e2
KDW79 <no1holycow@gmail.com>
2020-04-29 16:12:43 +0900

    test

--- testVS/testVS/Program.cs
+++ testVS/testVS/Program.cs
@@ -11,6 +11,7 @@ namespace testVS
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+	    	Console.WriteLine("Add this in git Bash");
         }
     }
 }
