4a42d4bbb02722b4db0f07e3f16a4100c4617c39
KDW79 <no1holycow@gmail.com>
2020-04-29 15:31:34 +0900

    Add it in git Bash

--- testVS/testVS/Program.cs
+++ testVS/testVS/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿iusing System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,7 @@ namespace testVS
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+	    Console.WriteLine("Add this in git Bash");
         }
     }
 }
